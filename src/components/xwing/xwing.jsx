002b3ffe80d149d5d6f55d6e07ff7c7a1e7b5508
Raphael <raphael.ferreira@hetic.net>
2023-03-11 20:46:42 +0100

    :art: xwing > center object

--- src/components/xwing/xwing.jsx
+++ src/components/xwing/xwing.jsx
@@ -3,11 +3,12 @@ import { useEffect } from "react"
 
 export default function Xwing() {
 	const xwing = useGLTF("./model/xwing/scene.gltf")
+	console.log(xwing)
 
 	return (
 		<primitive
 			object={xwing.scene}
-			position={[0, 0, 0]}
+			position={[-0.3, 0, 0]}
 			scale={0.01}
 			// rotation={[0, Math.PI / 2, 0]}
 		/>
